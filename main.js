60ba7387b9cb8fd119d97bc897cd6e5340a42467
agriye123 <88137750+agriye123@users.noreply.github.com>
2022-01-07 15:31:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,500);
+    canvas.position(560,160);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+
+    background('#D22B2B');
+
+    document.getElementById("square_sides").innerHTML="Width and Height of the square will be " + difference + "px";
+    fill('#000000');
+    stroke("white");
+    square(noseX, noseY, difference);
+}
+function modelLoaded()
+{
+    console.log("Posenet is initialised");
+}
+function gotPoses(results)
+{
+if(results.length>0)
+{
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("Nose x is "+ noseX + " y is " + noseY);
+    
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    console.log("Left wrist X = "+ leftWristX +" , right wrist x = "+ rightWristX + " , difference = " + difference);
+}
+}
